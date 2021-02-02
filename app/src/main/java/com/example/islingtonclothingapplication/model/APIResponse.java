8b9543ec657a109089a24fcd3778bc372b7a3bbb
Luv Jaiswal <42119313+LuvJaiswal@users.noreply.github.com>
2021-02-02 23:29:14 +0545

    API response and User model added

---
+++ app/src/main/java/com/example/islingtonclothingapplication/model/APIResponse.java
@@ -0,0 +1,45 @@
+package com.example.islingtonclothingapplication.model;
+
+public class APIResponse {
+        private boolean error;
+        private String uid;
+        private User user;
+        private String error_msg;
+
+
+    public APIResponse() {
+    }
+
+    public boolean isError() {
+        return error;
+    }
+
+    public void setError(boolean error) {
+        this.error = error;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getError_msg() {
+        return error_msg;
+    }
+
+    public void setError_msg(String error_msg) {
+        this.error_msg = error_msg;
+    }
+}
+
---
+++ app/src/main/java/com/example/islingtonclothingapplication/model/User.java
@@ -0,0 +1,51 @@
+package com.example.islingtonclothingapplication.model;
+
+class User {
+
+    private String name ;
+    private String email ;
+    private String created_at ;
+    private String updated_at = "" ;
+
+    public User() {
+    }
+
+    public User(String name, String email, String created_at, String updated_at) {
+        this.name = name;
+        this.email = email;
+        this.created_at = created_at;
+        this.updated_at = updated_at;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(String created_at) {
+        this.created_at = created_at;
+    }
+
+    public String getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(String updated_at) {
+        this.updated_at = updated_at;
+    }
+}
