8e78140415b28dc90e0612d900d0b20909a1a27e
Luv Jaiswal <42119313+LuvJaiswal@users.noreply.github.com>
2021-03-05 23:40:00 +0545

    navigation done successfully but menu item click not working

--- app/src/main/java/com/example/islingtonclothingapplication/HomeActivity.java
+++ app/src/main/java/com/example/islingtonclothingapplication/HomeActivity.java
@@ -1,11 +1,20 @@
 package com.example.islingtonclothingapplication;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.widget.LinearLayout;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
 import com.daimajia.slider.library.SliderLayout;
 import com.daimajia.slider.library.SliderTypes.BaseSliderView;
@@ -30,6 +39,7 @@ import io.reactivex.schedulers.Schedulers;
 import com.daimajia.slider.library.SliderLayout;
 import com.example.islingtonclothingapplication.model.Category;
 import com.example.islingtonclothingapplication.model.Clothes;
+import com.google.android.material.navigation.NavigationView;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -38,15 +48,58 @@ public class HomeActivity extends AppCompatActivity {
 
     RecyclerView lst_category;
 
+    private static final String TAG = "HomeActivity";
 
     //Rxjjava
     CompositeDisposable compositeDisposable = new CompositeDisposable();
 
+    //for toggle navigation
 
+    NavigationView navigationView;
+    ActionBarDrawerToggle toggle;
+    DrawerLayout drawerLayout;
+    androidx.appcompat.widget.Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //for navigation
+
+        androidx.appcompat.widget.Toolbar toolbar = (androidx.appcompat.widget.Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        navigationView =(NavigationView)findViewById(R.id.navmenu);
+        drawerLayout=(DrawerLayout)findViewById(R.id.drawer);
+
+        toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                switch (menuItem.getItemId()){
+                    case R.id.menu_home:
+                        Intent intent = new Intent(getApplicationContext(),RegisterActivity.class);
+                        startActivity(intent);
+                        Log.d(TAG,"homeclicked");
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+
+
+                    case R.id.cart_icon :
+                        Toast.makeText(getApplicationContext(), "Cart clicked", Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+                }
+
+                return true;
+            }
+        });
+
+
         mService = Common.getAPI();
 
         lst_category = (RecyclerView) findViewById(R.id.categoryList);
@@ -70,6 +123,7 @@ public class HomeActivity extends AppCompatActivity {
         //InitDatabase
         initDB();
     }
+
 
     private void initDB() {
         Common.cartDatabase = CartDatabase.getInstance(this);
@@ -104,7 +158,7 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     private void displayCategory(List<Category> categories) {
-        CategoryAdapter adapter = new CategoryAdapter(this,categories);
+        CategoryAdapter adapter = new CategoryAdapter(this, categories);
         lst_category.setAdapter(adapter);
     }
 
