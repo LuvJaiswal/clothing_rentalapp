91e7f1bd8c20f794042887d3abfdf1f8eb8359cb
Jeneliya Gurung <jeneliya@Jeneliyas-MacBook-Pro.local>
2021-03-29 13:57:51 +0545

    favourite list activity added

--- app/src/main/java/com/example/islingtonclothingapplication/FavouriteListActivity.java
+++ app/src/main/java/com/example/islingtonclothingapplication/FavouriteListActivity.java
@@ -1,14 +1,27 @@
 package com.example.islingtonclothingapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import io.reactivex.disposables.CompositeDisposable;
+
 public class FavouriteListActivity extends AppCompatActivity {
 
+    RecyclerView recycler_fav;
+
+    CompositeDisposable compositeDisposable;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favourite_list);
+
+        compositeDisposable = new CompositeDisposable();
+
+        recycler_fav = (RecyclerView)findViewById(R.id.recycler_fav);
+        recycler_fav.setLayoutManager(new LinearLayoutManager(this));
+        recycler_fav.setHasFixedSize(true);
     }
 }
