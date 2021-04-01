7ac77b10881879d829d5d008cd14914542e64369
Jeneliya Gurung <jeneliya@Jeneliyas-MacBook-Pro.local>
2021-04-01 13:19:29 +0545

    order model addded

---
+++ app/src/main/java/com/example/islingtonclothingapplication/model/Order.java
@@ -0,0 +1,67 @@
+package com.example.islingtonclothingapplication.model;
+
+public class Order {
+    private long orderId;
+    private int orderStatus;
+    private float orderPrice;
+    private String orderDetail, orderComment, orderAddress, userPhone;
+
+    public Order() {
+    }
+
+    public long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(long orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(int orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public float getOrderPrice() {
+        return orderPrice;
+    }
+
+    public void setOrderPrice(float orderPrice) {
+        this.orderPrice = orderPrice;
+    }
+
+    public String getOrderDetail() {
+        return orderDetail;
+    }
+
+    public void setOrderDetail(String orderDetail) {
+        this.orderDetail = orderDetail;
+    }
+
+    public String getOrderComment() {
+        return orderComment;
+    }
+
+    public void setOrderComment(String orderComment) {
+        this.orderComment = orderComment;
+    }
+
+    public String getOrderAddress() {
+        return orderAddress;
+    }
+
+    public void setOrderAddress(String orderAddress) {
+        this.orderAddress = orderAddress;
+    }
+
+    public String getUserPhone() {
+        return userPhone;
+    }
+
+    public void setUserPhone(String userPhone) {
+        this.userPhone = userPhone;
+    }
+}
